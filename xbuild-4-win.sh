293e20c29af40a03b47480acdf4fb051d49d917f
Mioki <22417711+okibcn@users.noreply.github.com>
2023-01-12 18:59:58 -0800

    New Github Action

--- xbuild-4-win.sh
+++ xbuild-4-win.sh
@@ -1,16 +1,24 @@
 #!/bin/bash -e
-sudo -E apt update && sudo apt upgrade -y
-sudo -E apt install -y autoconf automake autopoint gcc mingw-w64 gettext git groff make pkg-config texinfo p7zip-full
+# Copyright (c) 2022 okibcn
+# This is free software, licensed under the GNU General Public License v3.0
+# See /LICENSE for more information.
+# https://github.com/okibcn/nano-editor
+# Description: Build OpenWrt image with full config
+
+# Required packages:
+# sudo -E apt -qq update && sudo apt upgrade -y
+# sudo -E apt -qq install -y autoconf automake autopoint gcc mingw-w64 gettext git groff make pkg-config texinfo p7zip-full
 
 # Download sources
 git clone git://git.savannah.gnu.org/nano.git
 cd nano
-git log -n 1
-./autogen.sh
 git clone https://github.com/mirror/ncurses.git
+git log -n 1
 cd ncurses
 git log -n 1
+NCURSES=$(git show -s --format=%s)
 cd ..
+./autogen.sh
 
 # >realpath< function doesn't exist on Windows, which isn't fully POSIX compliant.
 echo " " >> ./src/definitions.h
@@ -21,16 +29,16 @@ echo "#endif" >> ./src/definitions.h
 # Change default terminal to nothing
 sed -i 's|vt220||g' ./src/nano.c
 
-# Dirty fix homedir detection
+# Fix homedir detection
 sed -i 's|\"HOME\"|"USERPROFILE\"|g' ./src/utils.c
 
 # Modify path expansion with backslashes
-cat src/files.c \
-  | sed "s,free(tilded);,free(tilded);\n\tfor(tilded = retval; \*tilded; ++tilded) if(\*tilded == 'SED_REPLACE') \*tilded = '/';,g" \
-  | sed "s,path\[i\] != '/',path[i] != '/' \&\& path[i] != 'SED_REPLACE',g"  \
-  | sed 's,SED_REPLACE,\\\\,g' > src/files2.c
-mv src/files2.c src/files.c
+sed -i -e "s,free(tilded);,free(tilded);\n\tfor(tilded = retval; \*tilded; ++tilded) if(\*tilded == '\\\\\\\\') \*tilded = '/';, ;
+           s,path\[i\] != '/',path[i] != '/' \&\& path[i] != '\\\\\\\\'," src/files.c
 
+# Adding static ncurses revision and patch level to nano version info.
+sed -i -e "s,Compiled options,Using ${NCURSES}\\\\n Compiled options," src/nano.c
+sed -i -e "s|git describe --tags 2>/dev/null|git describe --tags 2>/dev/null\` for Windows, build \`git rev-list --count HEAD|" src/Makefile.am
 
  ##########################
 ##                        ##
@@ -43,9 +51,9 @@ mkdir -p build/ncurses
 cd build/ncurses/
 ../../ncurses/configure --prefix="$(pwd)"  \
   --enable-{widec,sp-funcs,termcap,term-driver,interop}  \
-  --disable-{shared,database,rpath,home-terminfo,db-install,getcap}  \
-  --without-{progs,ada,cxx-binding,manpages,pthread,debug,tests,libtool}
-make -j$(($(nproc)*2))
+  --disable-{shared,database,rpath,home-terminfo,db-install,getcap,echo}  \
+  --without-{progs,ada,cxx-binding,manpages,pthread,debug,tests,libtool} || exit 1
+make -j$(($(nproc)*2)) || exit 1
 cd ../..
 
  ##########################
@@ -67,10 +75,10 @@ find . -type f  -name '*.*' | xargs rm -rf
 rm Makefile -rf
 ../../ncurses/configure --prefix="${OUTDIR}"  \
   --enable-{widec,sp-funcs,termcap,term-driver,interop}  \
-  --disable-{shared,database,rpath,home-terminfo,db-install,getcap}  \
-  --without-{progs,ada,cxx-binding,manpages,pthread,debug,tests,libtool}  --host="${TOOLCHAIN}" 
-make -j$(($(nproc)*2))
-make install
+  --disable-{shared,database,rpath,home-terminfo,db-install,getcap,echo}  \
+  --without-{progs,ada,cxx-binding,manpages,pthread,debug,tests,libtool}  --host="${TOOLCHAIN}"  || exit 1
+make -j$(($(nproc)*2)) || exit 1
+make install || exit 1
 cd ../..
 
 # Build nano
@@ -84,9 +92,9 @@ export NCURSESW_CFLAGS="-I\"${OUTDIR}/include/ncursesw\" -DNCURSES_STATIC"
 export NCURSESW_LIBS="-lncursesw"
 ../../configure --host="${TOOLCHAIN}" --prefix="${OUTDIR}"  \
   --enable-utf8 --disable-{nls,speller} \
-  --sysconfdir="C:\ProgramData"  
-make -j$(($(nproc)*2))
-make install-strip
+  --sysconfdir="C:\ProgramData"  || exit 1
+make -j$(($(nproc)*2)) || exit 1
+make install-strip || exit 1
 cd ../..
 
  ############################
@@ -108,10 +116,10 @@ find . -type f  -name '*.*' | xargs rm -rf
 rm Makefile -rf
 ../../ncurses/configure --prefix="${OUTDIR}"  \
   --enable-{widec,sp-funcs,termcap,term-driver,interop}  \
-  --disable-{shared,database,rpath,home-terminfo,db-install,getcap}  \
-  --without-{progs,ada,cxx-binding,manpages,pthread,debug,tests,libtool}  --host="${TOOLCHAIN}" 
-make -j$(($(nproc)*2))
-make install
+  --disable-{shared,database,rpath,home-terminfo,db-install,getcap,echo}  \
+  --without-{progs,ada,cxx-binding,manpages,pthread,debug,tests,libtool}  --host="${TOOLCHAIN}" || exit 1
+make -j$(($(nproc)*2)) || exit 1
+make install || exit 1
 cd ../..
 
 # Build nano
@@ -125,9 +133,9 @@ export NCURSESW_CFLAGS="-I\"${OUTDIR}/include/ncursesw\" -DNCURSES_STATIC"
 export NCURSESW_LIBS="-lncursesw"
 ../../configure --host="${TOOLCHAIN}" --prefix="${OUTDIR}"  \
   --enable-utf8 --disable-{nls,speller} \
-  --sysconfdir="C:\ProgramData"  
-make -j$(($(nproc)*2))
-make install-strip
+  --sysconfdir="C:\ProgramData"   || exit 1
+make -j$(($(nproc)*2))  || exit 1
+make install-strip  || exit 1
 cd ../..
 
  ############################
@@ -136,12 +144,12 @@ cd ../..
 ##                          ##
  ############################
 
-NANO_VERSION="$(git describe --tags || echo "v0.0.0-0-unknown")"
-NANO_REVISION="$(git rev-list --count HEAD)"
+NANO_VERSION="$(git describe --tags 2>/dev/null | sed "s/.\{10\}$//")-$(git rev-list --count HEAD)"
 
 strip -s pkg_{i686,x86_64}-w64-mingw32/bin/nano.exe
 cp doc/sample.nanorc.in .nanorc
 7z a -aoa -mmt"$(nproc)" --  \
-  "nano-editor_${NANO_REVISION}_${NANO_VERSION}.7z"  \
+  "nano-editor_${NANO_VERSION}.7z"  \
   pkg_{i686,x86_64}-w64-mingw32/{bin/nano.exe,share/{nano,doc}/}  \
-  .nanorc
+  .nanorc  || exit 1
+exit 0
